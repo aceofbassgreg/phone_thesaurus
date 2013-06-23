35e2464e303d6825bf3bc0db911ea36eacf24933
aceofbassgreg <gregory.kenenitz@gmail.com>
2013-06-23 15:39:19 -0400

    Switching to Poltergeist, having issues finding links

--- word_scraper.rb
+++ word_scraper.rb
@@ -1,33 +1,54 @@
-class WordRobot
+class WordScraper
 
   require 'rubygems'
-  require 'capybara/dsl'
+  require 'capybara'
+  require 'capybara/poltergeist'
+
+Capybara.javascript_driver = :poltergeist
+
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app)
+end
 
   def initialize
-    @s = WordRobot::Capybara::Session.new :selenium
+    @s = WordScraper::Capybara::Session.new :poltergeist
   end
 
   def session
     @s
   end
 
-  def all_words
-    []
-  end
-
-  def visit_site_and_scrape_words
+  def visit_site
     session.visit 'http://dictionary.reference.com/list/'
-    session.scrape_all_words
   end
 
-  def scrape_all_words
+  def screenshot            # troubleshooting helper :-)
+    session.driver.render('./file.png', :full => true)
+  end
+
+  def scrape_and_store_all_words
     links = session.all(:css, ".result_link").collect {|link| link.text}
+    all_words = []
 
     links.each do |link|
       session.click_link link
       words = session.all(:css, ".result_link").collect {|word| word.text}
       all_words << words
-      session.evaluate_script('window.history.back()')
+      visit_site
+    end
+    all_words.prune_and_upcase
+    all_words.map! {|word| element.gsub!(/[.,\ '&`~"$!?]/, '')}    #removing symbols and spaces from words
+    File.open("dictionary.txt","w") {|f| f.puts(all_words)}
+  end
+
+  def prune_and_upcase
+    all_words.each do |word|
+      if word.length > 7
+        all_words.delete(word)
+      else
+        word.upcase
+      end
     end
   end
+
 end
